aea7dda0f77c5f063d8cf39c36bdccc0d9f7ba2f
Juanmapp <juanma1694@gmail.com>
2023-06-16 18:40:34 -0300

    Agrego el manejo de errores

--- Clinica2/src/main/java/com/proyect/Clinica2/Clinica2Application.java
+++ Clinica2/src/main/java/com/proyect/Clinica2/Clinica2Application.java
@@ -1,5 +1,6 @@
 package com.proyect.Clinica2;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Clinica2Application {
 
 	public static void main(String[] args) {
+
+		PropertyConfigurator.configure("log4j.properties");
 		SpringApplication.run(Clinica2Application.class, args);
+
+
 	}
 
 }
